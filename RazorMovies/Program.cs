8454fde9daeda41696550243488ed7441326a4a1
Julio Guerrero Alvarez <116978348+julioguerrero131@users.noreply.github.com>
2025-04-09 11:39:56 -0500

    Movies Model and CRUD with Scaffolding

--- RazorMovies/Program.cs
+++ RazorMovies/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RazorMovies.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<RazorMoviesContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("RazorMoviesContext") ?? throw new InvalidOperationException("Connection string 'RazorMoviesContext' not found.")));
 
 var app = builder.Build();
 
